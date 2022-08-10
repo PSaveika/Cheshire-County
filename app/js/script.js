0496fbe614ff418b2f8d223afc62d79135614b17
Povilas Saveika <povilassaveika@gmail.com>
2022-08-10 23:58:37 +0300

    code cleanup

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,9 @@ const tabContent = document.getElementsByClassName("tab-content-wrapper");
 // Navigation variables
 const sections = document.querySelectorAll(".section");
 const navLinks = document.querySelectorAll("nav ul li");
+// Nav burger variables
+const burger = document.querySelector(".burger");
+const burgerList = document.getElementById("list");
 
 // Tabs
 for(var tab of tabContent) {
@@ -27,65 +30,31 @@ function changeTab(event, name) {
     event.currentTarget.classList.add("active-button");
 }
 
-//Nav on scroll
+// Nav on scroll
 window.addEventListener("scroll", function(){
-    let current = ""
+    let current = "";
    for(var section of sections){
-      const sectionTop = section.offsetTop
+      const sectionTop = section.offsetTop;
       if(window.pageYOffset >= sectionTop / 1.2) {
-         current = section.getAttribute("id")
+         current = section.getAttribute("id");
       }
    }
 
     for(var navLink of navLinks) {
         navLink.classList.remove("active");
-        navLink.classList.remove("lii");
         if(navLink.getAttribute("list-el-name") === current) {
             navLink.classList.add("active");
         }
     }
-    
-    
 })
 
-const burger = document.querySelector(".burger")
-const burgerList = document.getElementById("list")
-
+// Burger nav
 burger.addEventListener("click", function(){
-    // this.classList.toggle("rotate")
-    burgerList.classList.toggle("open")
- })
+    burgerList.classList.toggle("open");
+ });
 
 window.addEventListener("resize", function(){
     if(window.innerWidth > 768){
-       burger.classList.remove("rotate")
-       burgerList.classList.remove("open")
+       burgerList.classList.remove("open");
     }
- })
-
- console.log(burgerList)
-
-// Nav burger 
-
-// function myFunction() {
-//     var x = document.getElementById("list");
-//     console.log(x)
-//     if (x.style.display === "flex") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "flex";
-//     }
-//   } 
-
-
-// function getBurgerValue(burgerListElement, displayValue) {
-//     let burgerListStyle = window.getComputedStyle(burgerListElement);
-//     return burgerListStyle.getPropertyValue(displayValue)
-// }
-
-//dadaryt, kad ant click pasleptu
-// const burgerList = document.getElementById("list")
-//     let value = getBurgerValue(burgerList, "display");
-//     console.log(value);
-
-//     burgerList.style.setProperty("display" , "none")
+ });
